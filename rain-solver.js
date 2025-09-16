a214943eb9fae197bd3bc9dd11fd764ab1fb830d
rouzwelt <rouzweltt@gmail.com>
2025-09-16 01:12:31 +0000

    Update rain-solver.js

--- rain-solver.js
+++ rain-solver.js
@@ -1,23 +1,16 @@
-// #!/usr/bin/env node
+#!/usr/bin/env node
 
-// /* eslint-disable no-console */
-const { sleep } = require("./dist");
-// const { version } = require("./package.json");
+/* eslint-disable no-console */
+const { main } = require("./dist");
+const { version } = require("./package.json");
 
-// main(process.argv, version)
-//     .then(() => {
-//         console.log("\x1b[32m%s\x1b[0m", "Rain Solver process finished successfully!");
-//         process.exit(0);
-//     })
-//     .catch((v) => {
-//         console.log("\x1b[31m%s\x1b[0m", "An error occured during execution: ");
-//         console.log(v);
-//         process.exit(1);
-//     });
-(async () => {
-    const x = await sleep(1000).then(async () => {
-        await sleep(5000);
-        return 50;
+main(process.argv, version)
+    .then(() => {
+        console.log("\x1b[32m%s\x1b[0m", "Rain Solver process finished successfully!");
+        process.exit(0);
+    })
+    .catch((v) => {
+        console.log("\x1b[31m%s\x1b[0m", "An error occured during execution: ");
+        console.log(v);
+        process.exit(1);
     });
-    console.log(x);
-})();
