25155a510ae9d99fff10d79f20341f8e877c385f
rouzwelt <rouzweltt@gmail.com>
2025-10-17 15:41:52 +0000

    Update blacklist.ts

--- src/router/sushi/blacklist.ts
+++ src/router/sushi/blacklist.ts
@@ -5,6 +5,7 @@ export const BlackList = [
     "0x11E29b541AbE15984c863c10F3Ef9eCBcC078031",
     "0x59048Ff7D11ef18514163d0A860fb7A34927a452",
     "0x461053a473248054807241adE4F988A35899A312",
+    "0xB259dD2307E7626d925efb6D523b986b670e5631",
 ] as const;
 
 /** Blacklisted pools as a set, used by router */
