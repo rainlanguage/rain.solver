f80545b05cfa91a556f7b64948a7ba68eb4158e7
rouzwelt <rouzweltt@gmail.com>
2025-10-17 16:35:40 +0000

    Update blacklist.ts

--- src/router/sushi/blacklist.ts
+++ src/router/sushi/blacklist.ts
@@ -6,10 +6,10 @@ export const BlackList = [
     "0xFB957DE375cc10450D7a34aB85b1F15Ef58680b4",
     "0x11E29b541AbE15984c863c10F3Ef9eCBcC078031",
     "0x59048Ff7D11ef18514163d0A860fb7A34927a452",
-    "0x461053a473248054807241adE4F988A35899A312", // polygon quickswap nht/wpol
-    "0xB259dD2307E7626d925efb6D523b986b670e5631", // polygon sushiswap nht/wpol
-    "0x2344cD834d03bA94886ca5665F847BbDD7484f32", // polygon sushiswap nht/weth
-    "0xe427B62B495C1dFe1Fe9F78bEbFcEB877ad05DCE", // polygon sushiswap nht/usdt
+    "0x461053a473248054807241adE4F988A35899A312", // polygon quickswap NHT/WPOL
+    "0xB259dD2307E7626d925efb6D523b986b670e5631", // polygon sushiswap NHT/WPOL
+    "0x2344cD834d03bA94886ca5665F847BbDD7484f32", // polygon sushiswap NHT/WETH
+    "0xe427B62B495C1dFe1Fe9F78bEbFcEB877ad05DCE", // polygon sushiswap NHT/USDT
 ] as const;
 
 /** Blacklisted pools as a set, used by router */
